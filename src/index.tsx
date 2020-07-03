7b26c52a971a0d372bd435d923216e6c8e2e3cfa
Adam Belton <adambelton@outlook.com>
2020-07-03 21:51:26 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('root');
+
+const App = () => {
+	return <h1>I'm a scaffold for a cakes app!</h1>;
+};
+
+ReactDOM.render(<App />, root);
