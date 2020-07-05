8c4eaf17b88c4ab6ff584e9c97cc47935211f029
Adam Belton <adambelton@outlook.com>
2020-07-05 15:47:25 +0100

    replace renamed import

--- src/@pages/vote.tsx
+++ src/@pages/vote.tsx
@@ -1,11 +1,11 @@
 import { CatPreview } from '@components/cat-preview';
 import { PreviewSize } from '@components/types';
-import { IRandomCat, useCats } from '@hooks';
+import { IImageCat, useCats } from '@hooks';
 import React from 'react';
 
 export const Vote: React.FC = () => {
     const { getRandomCat, setFavourite } = useCats();
-    const [randomCat, setRandomCat] = React.useState<null | IRandomCat>(null);
+    const [randomCat, setRandomCat] = React.useState<null | IImageCat>(null);
 
     React.useEffect(() => {
         getRandomCat(setRandomCat);
